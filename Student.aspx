b61f9c80cff4da9b7a262360d8e9d7bbe6ad6fe8
mahamudul-nobi-mohon <mn.mohon@gmail.com>
2017-01-02 00:44:41 +0600

    new

---
+++ Student.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Student.aspx.cs" Inherits="Student" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="border: 1px solid black; font-family: arial">
+                <tr>
+                    <td>Select chart : </td>
+                    <td>
+                        <asp:DropDownList ID="DropDownList1" AutoPostBack="True" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Chart ID="Chart1" runat="server">
+                            <Series>
+                                <asp:Series Name="Series1" ChartArea="ChartArea1" ChartType="Pie"></asp:Series>
+                            </Series>
+                            <ChartAreas>
+                                <asp:ChartArea Name="ChartArea1"></asp:ChartArea>
+                            </ChartAreas>
+                        </asp:Chart>
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Student.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.DataVisualization.Charting;
+using System.Web.UI.WebControls;
+
+public partial class Student : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            GetChartTypes();
+            GetChartData();
+        }
+    }
+
+    private void GetChartTypes()
+    {
+        foreach (int chartType in Enum.GetValues(typeof(SeriesChartType)))
+        {
+            ListItem li = new ListItem(Enum.GetName(typeof(SeriesChartType),chartType),chartType.ToString());
+            DropDownList1.Items.Add(li);
+        }
+    }
+
+    void GetChartData()
+    {
+        string cs = ConfigurationManager.ConnectionStrings["Chart"].ConnectionString;
+        Series series = Chart1.Series["Series1"];
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+             SqlCommand cmd = new SqlCommand("Select studentName,TotalMarks from Students",con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                series.Points.AddXY(reader["StudentName"].ToString(), reader["TotalMarks"]);
+            }
+        }
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        GetChartData();
+        Chart1.Series["Series1"].ChartType =(SeriesChartType)Enum.Parse(typeof (SeriesChartType), DropDownList1.SelectedValue);
+    }
+}
